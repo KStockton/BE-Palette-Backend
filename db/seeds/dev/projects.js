813ef43ee000a34296eb158f95ce97c7f8ff715d
Michael KS <kingstockton@gmail.com>
2019-07-02 16:37:45 -0600

    Complete seeding for projects and palettes
    Co-authored-by: Kristen Hallstrom <klhalls89@gmail.com>

--- db/seeds/dev/projects.js
+++ db/seeds/dev/projects.js
@@ -1,15 +1,32 @@
 const projectsData = require('../../../seedData')
 
+const createProject = (knex, project) => {
+  return knex('projects').insert({
+    project_title: project.project_title
+  },'id')
+  .then(projectId => {
+    const palettesPromises = [];
 
+    project.palettes.forEach(palette => {
+      palettesPromises.push(
+        createPalette(knex, {
+          palette_title: palette.palette_title,
+          color_1: palette.color_1,
+          color_2: palette.color_2,
+          color_3: palette.color_3,
+          color_4: palette.color_4,
+          color_5: palette.color_5,
+          project_id: projectId[0]
+        })
+      )
+    })
+    return Promise.all(palettesPromises)
+  })
+};
 
-
-
-
-
-
-
-
-
+const createPalette = (knex, palette) => {
+  return knex('palettes').insert(palette)
+};
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
