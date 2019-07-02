8eba8cd758e2b7223e471a6269fabca62450e8ca
Michael KS <kingstockton@gmail.com>
2019-07-02 16:20:42 -0600

    Add seed function for project promies
    Co-authored-by: Kristen Hallstrom <klhalls89@gmail.com>

--- db/seeds/dev/projects.js
+++ db/seeds/dev/projects.js
@@ -1,6 +1,28 @@
-const projectData = require('../../../seedData')
+const projectsData = require('../../../seedData')
+
+
+
+
+
+
+
+
+
+
+
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  
+  return knex('palettes').del()
+    .then(() => knex('projects').del())
+    .then(() =>  {
+      const projectPromise = []
+
+      projectsData.forEach(project => {
+        projectPromise.push(createProject(knex, project))
+      });
+      
+      return Promise.all(projectPromise)
+    })
+    .catch(error => console.log(`Error seeding data: ${error}`));
 };
