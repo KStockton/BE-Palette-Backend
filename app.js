a8e84808a5ef6074710c661090b6dbf22b817373
Michael KS <kingstockton@gmail.com>
2019-07-02 13:51:30 -0600

    add travis.yml

---
+++ app.js
@@ -0,0 +1,19 @@
+// const environment = process.env.NODE_ENV || 'development'
+// const configuration= require('./knexfile')[environment]
+// const database = require('knex')(configuration)
+const express = require('express');
+// const bodyParser = require('body-parser');
+const app = express();
+// app.use(bodyParser.json())
+
+const PORT = process.env.PORT || 3000
+
+app.use(express.json())
+
+app.get('/', (request, response) => {
+  response.send('We\'re going to test all the routes!');
+});
+
+app.listen(PORT, () => {
+  console.log(`App is running 👻 on port ${PORT}`)
+});
