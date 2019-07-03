114fbe74b2dc94607ffdf3c2c8ea50bd05a23ff1
Kristen Hallstrom <klhalls89@gmail.com>
2019-07-03 14:30:43 -0600

    Add get project by id endpoint in server

--- app.js
+++ app.js
@@ -20,33 +20,28 @@ app.get('/', (request, response) => {
   response.send('We\'re going to test all the routes!');
 });
 
-// app.get('/api/v1/projects',  (request, response) => {
-//   database('projects').select()
-//   .then(projects => {
-//     if(projects.length) {
-//       return response.status(200).json(projects)
-//     } else {
-//       return response.status(404).json('Not Found')
-//     }
-//   })
-//   .catch((error) => 
-//     response.status(500).json({error}))
-// });
-
-
 app.get('/api/v1/projects', async (request, response) => {
   try {
     const projects = await database('projects').select()
       if(projects.length) return response.status(200).json(projects)
       if(!projects.length) return response.status(404).json('Not Found')
-    } catch(error) {
+  } catch(error) {
       return response.status(500).json({error})
     }
 })
 
-
-
-
+app.get('/api/v1/projects/:id', async (request, response) => {
+  const {id} = request.params
+  console.log(id)
+  try{
+    const project = await database('projects').where('id', id).select()
+    console.log(project)
+      if(project.length) return response.status(200).json(project)
+      if(!project.length) return response.status(404).json(`{Error: No palette found with ${id}}`)
+  } catch(error) {
+    return response.status(500).json({error})
+  }
+})
 
 //Palettes
 
@@ -56,7 +51,7 @@ app.get('/api/v1/palettes', async (request, response) => {
       if(palettes.length) return response.status(200).json(palettes)
       if(!palettes.length) return response.status(400).json('Not Found')
   } catch(error) {
-    return response.status(500).json({error})
+      return response.status(500).json({error})
   }
 })
 
@@ -65,7 +60,7 @@ app.get('/api/v1/palettes/:id', async (request, response) => {
   try{
     const palette = await database('palettes').where('id', id).select()
     if(palette.length) return response.status(200).json(palette)
-    if(!palette.length) return response.status(404).json(`{Error: No palette found with ${id}`)
+    if(!palette.length) return response.status(404).json(`{Error: No palette found with ${id}}`)
   } catch(error) {
     return response.status(500).json({error})
   }
