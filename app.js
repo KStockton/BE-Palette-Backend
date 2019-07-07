b7b17b891e96ac64f35c6858b42d9d71423cf708
Michael KS <kingstockton@gmail.com>
2019-07-06 19:40:17 -0600

    removed for heroku

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const environment = process.env.NODE_ENV || 'development'
 const configuration= require('./knexfile')[environment]
 const database = require('knex')(configuration);
 const express = require('express');
-// const bodyParser = require('body-parser');
 const app = express();
-// app.set("port", process.env.PORT || 3001)
 
 
 const cors = require('cors');
