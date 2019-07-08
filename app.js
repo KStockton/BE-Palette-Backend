305d5d72316506622ace4b72b535ca1ea6ac3b73
Michael KS <kingstockton@gmail.com>
2019-07-07 22:36:21 -0600

    heroku error fixing

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 app.use(express.json())
 
-app.get('/', async (req, response) => {
+app.get('/', (request, response) => {
   return response.status(200).json('Ready to begin')
 })
 
