c745bfb6b68e2ad208241fcf93a09d2c0514a81a
Michael KS <kingstockton@gmail.com>
2020-01-17 03:32:06 -0500

    npm fix

---
+++ libs/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken')
+
+export function verifyJWTToken(token);
+
+{
+  return new Promise((resolve, reject) =>
+  {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => 
+    {
+      if (err || !decodedToken)
+      {
+        return reject(err)
+      }
+
+      resolve(decodedToken)
+    })
+  })
+}
